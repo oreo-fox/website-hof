dee1a6be1dcbe9b792e89e61bb2098dbb3ae9413
Anna <anna.albrecht91@gmail.com>
2021-03-10 23:33:27 +0100

    first

---
+++ scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+(function () {
+    function init() {
+        var router = new Router([
+            new Route('home', 'home.html', true),            
+            new Route('about', 'about.html'),
+            new Route('bbq', 'bbq.html'),
+            new Route('partner', 'partner.html'),
+            new Route('spycher', 'spycher.html'),
+            new Route('wandern', 'wandern.html')
+
+        ]);
+    }
+    init();
+}());
